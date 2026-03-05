73077edae23c42b3b232a7e3cdd72f40a17c107f
AkshayenJeyamaran <akshayen.jeyamaran@student.tdsb.on.ca>
2026-03-05 15:04:16 +0000

    Finish questions 1 and 2

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,6 +1,70 @@
-public class PracticeProblem {
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
 
-	public static void main(String args[]) {
-		
-	}
+public class PracticeProblem {
+    public static String getName(int line, String file) {
+        try {
+            Scanner sc = new Scanner(new File(file));
+            int count = 1;
+            while (sc.hasNextLine()) {
+                String current = sc.nextLine();
+
+                if (count == line) {
+                    String[] parts = current.split(" ");
+                    sc.close();
+                    return parts[0] + " " + parts[1];
+                }
+                count++;
+            }
+            sc.close();
+        } catch (Exception e) {}
+        return "";
+    }
+    public static int getAge(int line, String file) {
+        try {
+            Scanner sc = new Scanner(new File(file));
+            int count = 1;
+
+            while (sc.hasNextLine()) {
+                String current = sc.nextLine();
+                if (count == line) {
+                    String[] parts = current.split(" ");
+                    sc.close();
+                    return Integer.parseInt(parts[2]);
+                }
+                count++;
+            }
+
+            sc.close();
+        } catch (Exception e) {}
+        return -1;
+    }
+    public static int getNumber(int line, String file) {
+        try {
+            Scanner sc = new Scanner(new File(file));
+            int count = 1;
+
+            while (sc.hasNextLine()) {
+                String current = sc.nextLine();
+
+                if (count == line) {
+                    String[] parts = current.split(" ");
+                    sc.close();
+                    return Integer.parseInt(parts[3]);
+                }
+                count++;
+            }
+            sc.close();
+        } catch (Exception e) {}
+        return -1;
+    }
+    public static void fileAppend(String output, String filename) {
+        try {
+            FileWriter fw = new FileWriter(filename, true);
+            fw.write(output);
+            fw.close();
+        } catch (IOException e) {}
+    }
 }
